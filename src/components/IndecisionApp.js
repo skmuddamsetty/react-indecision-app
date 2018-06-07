714071f0ed3624cbd64a7deb99861390698bd394
skmuddamsetty <muddamsetty.santhosh@gmail.com>
2018-06-07 08:05:40 -0400

    Partial Styles

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,51 +5,53 @@ import Action from './Action';
 import Options from './Options';
 import OptionModal from './OptionModal';
 export default class IndecisionApp extends React.Component {
-    state ={
+    state = {
         options: [],
         selectedOption: undefined
     };
-  handleDeleteOptions = () => {
-    this.setState(() => ({ options: [] }));
-  }
-  handlePick = () => {
-    const randomNum = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomNum];
-    this.setState(()=>({selectedOption:option}));
-  }
-  handleAddOption= (option) => {
-    if (!option) {
-      return 'Enter Valid value to add item!';
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists!';
-    } else {
-      this.setState((prevState) => {
-        return {
-          options: prevState.options.concat([option])
-        };
-      });
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: [] }));
     }
-  }
-  handleDeleteOption = (optionToRemove) => {
-    this.setState((prevState) => ({ options: prevState.options.filter(option => optionToRemove !== option) }));
-  }
-  clearSelectedOption = () => {
-      this.setState(()=>({selectedOption: undefined}));
-  }
-  render() {
-    const title = "Indecision App";
-    const subtitle = "Put your life into hands of a computer";
-    return (
-      <div>
-        <Header title={title} subtitle={subtitle} />
-        <Action hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick} />
-        <Options options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption} />
-        <AddOption handleAddOption={this.handleAddOption} />
-        <OptionModal selectedOption={this.state.selectedOption} clearSelectedOption={this.clearSelectedOption}/>
-      </div>
-    );
-  };
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        this.setState(() => ({ selectedOption: option }));
+    }
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter Valid value to add item!';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists!';
+        } else {
+            this.setState((prevState) => {
+                return {
+                    options: prevState.options.concat([option])
+                };
+            });
+        }
+    }
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({ options: prevState.options.filter(option => optionToRemove !== option) }));
+    }
+    clearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }));
+    }
+    render() {
+        const title = "Indecision App";
+        const subtitle = "Put your life into hands of a computer";
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <div className="container">
+                    <Action hasOptions={this.state.options.length > 0}
+                        handlePick={this.handlePick} />
+                    <Options options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption={this.handleDeleteOption} />
+                    <AddOption handleAddOption={this.handleAddOption} />
+                </div>
+                <OptionModal selectedOption={this.state.selectedOption} clearSelectedOption={this.clearSelectedOption} />
+            </div>
+        );
+    };
 }
